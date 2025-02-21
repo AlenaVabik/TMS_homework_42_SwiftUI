821d4dcc47815fccf241f8787fce07c00810edaa
Alena Vabishchevich <pilet93@mail.ru>
2025-02-21 21:58:29 +0300

    create-project

---
+++ hw_42_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hw_42_SwiftUI
+//
+//  Created by Alena  on 21.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hw_42_SwiftUI/hw_42_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  hw_42_SwiftUIApp.swift
+//  hw_42_SwiftUI
+//
+//  Created by Alena  on 21.02.25.
+//
+
+import SwiftUI
+
+@main
+struct hw_42_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
