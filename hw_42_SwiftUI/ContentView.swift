a98d4de7fc5683617abc012d013e94b05a577687
Alena Vabishchevich <pilet93@mail.ru>
2025-02-23 09:22:04 +0300

    create-hw-42

--- hw_42_SwiftUI/ContentView.swift
+++ hw_42_SwiftUI/ContentView.swift
@@ -8,15 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isHeartSelected: Bool = false
+    
     var body: some View {
+        ZStack(alignment: .topTrailing) {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Image("Image_iPhone16pro")
+                .resizable()
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .frame(maxWidth: 250, maxHeight: 320, alignment: .center)
+            HStack {
+                Text("$999")
+                    .bold()
+                    .font(.system(size: 35))
+                //                    .padding(.leading)
+                Text("$1099")
+                    .font(.system(size: 20))
+                    .foregroundColor(.gray)
+                    .strikethrough()
+            }
+            .frame(width: 240, height: 50, alignment: .leading)
+            
+            Text("Apple iPhone 16 Pro 128GB")
+                .frame(width: 240, height: 20, alignment: .leading)
+            
+            
         }
-        .padding()
+        Button(action: {
+            self.isHeartSelected.toggle()
+        })  {
+            ZStack {
+                Image(systemName: "circle.fill")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .foregroundColor(.white)
+                if isHeartSelected {
+                    Image(systemName: "heart.fill")
+                        .resizable()
+                        .frame(width: 30, height: 27)
+                        .foregroundColor(.red)
+                } else {
+                    Image(systemName: "heart")
+                        .resizable()
+                        .frame(width: 30, height: 27)
+                        .foregroundColor(.red)
+                }
+            }
+        }
+        .offset(x: 20, y: 0)
+//        .padding()
     }
+                   }
 }
 
 #Preview {
